d7090be091227cc68a16b9aeeebc765eb3a7d7a4
Parsa307 <79400634+Parsa307@users.noreply.github.com>
2025-11-26 15:47:06 +0330

    (fix): Another attempt for correcting the coloring.

--- app/src/main/java/com/relgl/modmenu/Menu.java
+++ app/src/main/java/com/relgl/modmenu/Menu.java
@@ -745,8 +745,8 @@ public class Menu {
                 GD_THUMB.setStroke(2, Color.WHITE);
                 GD_THUMB.setColor(LST_MAB);
             } else {
-                GD_TRACK.setStroke(2, Color.WHITE);
-                GD_TRACK.setColor(LST_MAB);
+                GD_TRACK.setStroke(2, LST_MAB);
+                GD_TRACK.setColor(Color.WHITE);
 
                 GD_THUMB.setStroke(2, LST_MAB);
                 GD_THUMB.setColor(Color.WHITE);
